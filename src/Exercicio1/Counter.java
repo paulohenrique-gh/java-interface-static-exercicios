fa792e0cef12eaaf2862b730e757c88ebf8998b5
Paulo Henrique <ph.1928@hotmail.com>
2024-07-01 21:09:24 -0300

    Finish exercise 1

---
+++ src/Exercicio1/Counter.java
@@ -0,0 +1,13 @@
+package Exercicio1;
+
+public class Counter {
+    public static int count = 0;
+
+    public Counter() {
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+}
---
+++ src/Exercicio1/Main.java
@@ -0,0 +1,14 @@
+package Exercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Current count: " + Counter.getCount());
+
+        System.out.println("Creating new instance...");
+        new Counter();
+        System.out.println("Current count: " + Counter.getCount());
+        System.out.println("One more...");
+        new Counter();
+        System.out.println("Current count: " + Counter.getCount());
+    }
+}
